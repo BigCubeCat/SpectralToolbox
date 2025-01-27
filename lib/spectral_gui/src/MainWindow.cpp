788c8ba08b35c5e42bd9d3865a245daa8dc6df2c
Биточкин Егор <e.bitochkin@g.nsu.ru>
2025-01-27 18:52:00 +0700

    feat(cmake): немного настроил cmake

---
+++ lib/spectral_gui/src/MainWindow.cpp
@@ -0,0 +1,9 @@
+#include "MainWindow.hpp"
+
+#include <QWidget>
+
+
+main_window::main_window(QWidget *parent)
+    : QMainWindow(parent) {    // m_ui(std::make_unique<Ui::MainWindow>()) {
+    // m_ui->setupUi(this);
+}
