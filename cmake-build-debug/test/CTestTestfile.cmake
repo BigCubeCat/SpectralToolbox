6f98f34154163ab3cf2c7dae7a39091e34d00694
AMChumak <i@amchumak.ru>
2025-01-31 01:06:28 +0700

    deleted rubbish

--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anton/Documents/geohack/SpectralToolbox/test
-# Build directory: /home/anton/Documents/geohack/SpectralToolbox/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[test_spectral_toolbox]=] "/home/anton/Documents/geohack/SpectralToolbox/cmake-build-debug/test/test_spectral_toolbox")
-set_tests_properties([=[test_spectral_toolbox]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/anton/Documents/geohack/SpectralToolbox/test/CMakeLists.txt;13;add_test;/home/anton/Documents/geohack/SpectralToolbox/test/CMakeLists.txt;0;")
